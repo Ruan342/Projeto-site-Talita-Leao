66be116fbf07af159cd18676f5dbaee98695c572
Ruan <ruan342.com@gmail.com>
2023-03-23 14:39:18 -0300

    arquivos referente ao site Talita Leao

---
+++ backend.js
@@ -0,0 +1,32 @@
+var mybutton = document.getElementById("sm");
+
+// quando rolar a página 20px do topo para baixo, o botão será exibido
+
+
+
+
+
+// // quando clicar no botão, a página rola para o topo do documento
+function smFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 690;
+}
+
+function agFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 1355;
+  }
+  function cgFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 2885;
+  }
+  function QAMFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 2185;
+  }
+  function endFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+ 
